9b145cad8cf1e6c8ac2a8e142b27da336236a037
unknown <fritzc1@CPSUNITY-07.active.newpaltz.edu>
2015-03-05 08:46:15 -0500

    first commit of useless application

---
+++ WpfApplication1/StringCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1
+{
+    class StringCalculator
+    {
+        public static string AddTwoStrings(string str1, string str2)
+        {
+            return (double.Parse(str1.Text) + double.Parse(str2.Text)).ToString();
+        }
+    }
+}
